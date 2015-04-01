9f8674a701b4bf2501ff546edb99cadfde907a9b
Veremey <Veremey@i.ua>
2015-04-02 01:57:06 +0300

    768

--- js/common.js
+++ js/common.js
@@ -14,6 +14,21 @@ head.ready(function() {
 	// $(window).scroll(function(){
 	//     scrollFixedElements()
 	// });
+$(document).ready(function() {
+	$('.nav').addClass('open');
+$('.bt-nav').click( function() {
+	$(this).parent().toggleClass( function() {
+      if ( $(this).hasClass('open') ) {
+        return 'close';
+      } else
+      if ( $(this).hasClass('close') ) {
+        return 'open';
+      }
+    });
+  });
+
+
+});
 
 	console.log($('body').html());
 });
